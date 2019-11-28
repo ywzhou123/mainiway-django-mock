b2f2f4f1ef8ea057292916ef1cd8d106216e3661
周有为 <ywzhou@youshiker.com>
2019-11-28 10:05:57 +0800

    no message

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 sudo docker stop mock
 sudo docker rm mock
-sudo docker run -p 8004:8000 --name mock --restart always --link mysql:localhost --link redis:localhost  -d django-mock
+sudo docker run -p 8004:8000 --name mock --restart always --link mysql:localhost -d django-mock
